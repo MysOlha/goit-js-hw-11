4130795dcdf8b17b7a2b9b93c5355bc95a107d4b
MysOlha <99076607+MysOlha@users.noreply.github.com>
2022-09-07 12:44:28 +0000

    Deploying to gh-pages from @ MysOlha/goit-js-hw-11@93e45be4e0c8fbb8439a5d85f575be8b86bcd210 🚀

---
+++ index.74154e6b.js
@@ -0,0 +1,2 @@
+console.log("hello");fetch("https://pixabay.com/api/q=cat, options").then((o=>o.json())).then(console.log);
+//# sourceMappingURL=index.74154e6b.js.map
---
+++ index.b7710f03.js
@@ -0,0 +1,2 @@
+!function(){console.log("hello");fetch("https://pixabay.com/api/q=cat, options").then((function(o){return o.json()})).then(console.log)}();
+//# sourceMappingURL=index.b7710f03.js.map
